4cee1e4d999a80d376af1e5adc928ace017fb6a6
Evan Stroud <evan.stroud@wework.com>
2018-07-05 22:48:19 +0000

    automatically backed up by learn

---
+++ hello.rb
@@ -0,0 +1,18 @@
+def say_hello
+  phrase = "hello"
+  puts phrase
+end
+
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
---
+++ iPhone_Models.rb
@@ -0,0 +1,3 @@
+iphone_models = ["1", "3", "3GS", "4", "4S", "5", "5S", "6", "6Plus", "7", "7Plus", "8", "8Plus", "X"]
+
+puts iphone_models
